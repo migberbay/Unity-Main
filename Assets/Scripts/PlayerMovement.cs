2462b26231096dffa4e939ca307a3c35fd74921f
migberbay <migberbay@gmail.com>
2019-03-14 01:57:20 +0100

    sorta done player controls structure and some more method testing

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Jump")){
+            Debug.Log("YOU PRESSED DOWN SPACEBAR");
+        }else if (Input.GetButton("Jump")){
+            Debug.Log("YOU ARE PRESSING SPACEBAR");
+        }else if (Input.GetButtonUp("Jump")){
+            Debug.Log("YOU RELEASED SPACEBAR");
+        }
+
+        if(h<0){
+            Debug.Log("moving left");
+        }else if(h>0){
+            Debug.Log("moving right");
+        }
+
+        if(v<0){
+            Debug.Log("moving down");
+        }else if(v>0){
+            Debug.Log("moving up");
+        }
+        
+        if(Input.GetKeyDown(KeyCode.N)) {
+            Debug.Log("ARE U TYPING THE N-WORD? YOU FUCKING NIGGER");
+        }
+
+        
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Test : MonoBehaviour
+{
+    public int publicNum;
+    private int privateNum;
+    
+
+    // Start is called before the first frame update
+    void Start(){
+      GameObject player = GameObject.FindGameObjectWithTag("Player");
+      GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+      GameObject boss = GameObject.Find("BOSS");
+      Debug.Log(boss);
+      Debug.Log(player);
+      for (int i = 0; i < 4; i++)
+      {
+          Debug.Log(enemies[i]);
+      }
+      Invoke("method", 1.0f);
+      InvokeRepeating("method", 3.0f, 5.0f);
+    }
+
+    // Update is called once per frame
+    void Update(){
+      
+    }
+
+    private void Awake() {
+        
+    }
+
+    private void FixedUpdate() {
+        //This should be used for calculations that need to occur regularly
+        //for example object motion
+        if(privateNum>3){
+          CancelInvoke();
+        }
+       
+    }
+
+    private void method(){
+      Debug.Log("I HAVE A BIG PEEPEEE" + Time.time);
+      privateNum++;
+    }
+}
